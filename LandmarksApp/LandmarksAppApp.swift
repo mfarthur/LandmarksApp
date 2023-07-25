dc25771d4ade42a06f4da1f462eadd4a993e4189
Arthur Martins Freire <mfarthurr@gmail.com>
2023-07-25 16:09:57 -0300

    Initial Commit

---
+++ LandmarksApp/LandmarksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksAppApp.swift
+//  LandmarksApp
+//
+//  Created by Arthur Martins on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
